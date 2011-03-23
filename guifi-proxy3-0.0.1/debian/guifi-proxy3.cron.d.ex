047210e0ba097257a5cfb154471ba51777636479
Pere Casas Puig <casas@Phoenix.(none)>
2011-03-23 19:44:35 +0100

    Delete some files of branch guifi-proxy3 Add some files of brach guifi-proxy3

--- guifi-proxy3-0.0.1/debian/guifi-proxy3.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the guifi-proxy3 package
-#
-0 4	* * *	root	[ -x /usr/bin/guifi-proxy3_maintenance ] && /usr/bin/guifi-proxy3_maintenance
--- guifi-proxy3-0.0.1/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(guifi-proxy3):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="guifi-proxy3" command="/usr/bin/guifi-proxy3"
