8561dfe5e16c8da4542fa096cb519e86a0bde387
Pere Casas Puig <pcasas.pere@gmail.com>
2011-04-22 17:30:56 +0200

    fix cron problem

--- guifi-proxy3-0.0.1/debian/guifi-proxy3.cron.d
+++ guifi-proxy3-0.0.1/debian/guifi-proxy3.cron.d
@@ -1,4 +1,5 @@
 #
 # Regular cron jobs for the guifi-proxy3 package
 #
-55 *	* * *	root	/usr/share/guifi-proxy3/guifi-proxy3.sh >> /var/log/guifi-proxy3/guifi-proxy3.log 2>&1;
+55 * * * *	root	/usr/share/guifi-proxy3/guifi-proxy3.sh >> /var/log/guifi-proxy3/guifi-proxy3.log 2>&1;
+0 5 * * *	root	/usr/share/guifi-proxy3/squid_cache >> /var/log/squid3/squid_cache.log 2>&1;
